4e97ec3bf1195375f1a37ca052223d0e6496f2f6
Tyler Baxter <tybaxter@calpoly.edu>
2023-12-01 11:05:56 -0800

    1237

--- lab6.java
+++ lab6.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 2KB, direct mapped, 2-word blocks
 2KB, direct mapped, 4-word blocks
 2KB, 2-way set associative, 1-word blocks
-2KB, 4-way set associative, 1-word blocks
+2KB, 4-way set associative, 1-word blocksv
 2KB, 4-way set associative, 4-word blocks
 4KB, direct mapped, 1-word blocks
 */
